cd8ad17a47fcb54310ee1986366e8562e8824cc4
bonzokoles <karollisson.ams@gmail.com>
2025-08-26 23:48:32 +0200

    Migrate to new Astro site design with Tailwind, Svelte and Cloudflare Workers support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,25 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
+import { siteUrl } from "./src/data/site.json";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [svelte(), mdx(), sitemap()],
   adapter: cloudflare({
     platformProxy: {
       enabled: true,
     },
   }),
+  site: siteUrl,
+  redirects: {
+    "/posts": "/", // redirect from /posts because that page doesn't exist.
+  },
 });
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
-import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { siteTitle, SiteDescription } from "../data/site.json";
 
 export async function GET(context) {
-  const posts = await getCollection("blog");
+  const posts = await getCollection("posts");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: siteTitle,
+    description: SiteDescription,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.id}/`,
+      link: `/posts/${post.id}/`,
     })),
   });
 }
